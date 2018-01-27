c743d27499588793d74b7654577c9245e3baee33
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-01-27 08:33:27 -0600

    Se añadió el modelo de datos para una persona simple

--- server/models/Person.js
+++ server/models/Person.js
@@ -1,4 +1,20 @@
 'use strict'
 export class Person {
-   
+   constructor(name, lastName) {
+       this.name = name
+       this.lastName = lastName
+   }
+
+   get Name() {
+       return this.name
+   }
+   set Name(name){
+       this.name = name
+   }
+   get LastName() {
+       return this.lastName
+   }
+   set LastName(lastName) {
+       this.lastName = lastName
+   }
 }
