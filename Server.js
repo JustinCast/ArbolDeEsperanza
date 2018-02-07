8df912e61c19970329c023d31235a32548680adf
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-07 11:05:08 -0600

    Changes to deploy

--- Server.js
+++ Server.js
@@ -1,17 +1,9 @@
+//Install express server
 const express = require('express');
 const app = express();
-const path = require('path')
-// Run the app by serving the static files
-// in the dist directory
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-// pathlocationstrategy
-
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname +  '/dist/index.html'))
-// })
-
-console.log('Console listening')
