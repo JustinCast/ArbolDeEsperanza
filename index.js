7a46370379e0a8a6f59d385f0a9843859ba9e85e
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-11 16:44:09 -0600

    testing

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
 const app = express();
-const path = require('path')
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
 
-// pathlocationstrategy
+// API file for interacting with MongoDB
+const api = require('./server/routes/api');
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +  '/dist/index.html'))
-})
+// Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
 
-console.log('Console listening')
+// Angular DIST output folder
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// API location
+app.use('/api', api);
+
+// Send all other requests to the Angular app
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set Port
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
