a76ca64db2fb1e5073c17a9af12b4e0e32cd4e24
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-11 16:36:31 -0600

    testing

--- index.js
+++ index.js
@@ -1,35 +1,18 @@
-// Get dependencies
+
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes/api');
-
 const app = express();
-
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
+const path = require('path')
+// Run the app by serving the static files
+// in the dist directory
 app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
 
-// Set our api routes
-app.use('/person', api);
+// pathlocationstrategy
 
-// Catch all other routes and return the index file
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +  '/dist/index.html'))
+})
 
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(process.env.PORT || 8080, () => console.log(`API running on localhost:${port}`));
+console.log('Console listening')
