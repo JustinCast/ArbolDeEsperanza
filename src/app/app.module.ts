6c16d034c7c6b53177e6d695ced270dd9fcf44e7
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-12 16:56:50 -0600

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -42,7 +42,6 @@ import { PeopleComponent } from './people/people.component';
 import { EditPersonComponent } from './edit-person/edit-person.component';
 import { LoginComponent } from './login/login.component';
 import { SearchFilterPipe } from "./show-people/SearchFilterPipe";
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
 // registerLocaleData(es, 'Es');
 @NgModule({
   declarations: [
@@ -85,7 +84,6 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
     MatListModule,
     MatIconModule,
     MatAutocompleteModule,
-    Ng2SearchPipeModule
   ],
   providers: [
     PeopleService
