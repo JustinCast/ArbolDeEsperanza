e1807dcbe7454dba8722829f021625a2b4949942
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-12 17:51:42 -0600

    Refactorizing show people

--- src/app/show-people/show-people.component.ts
+++ src/app/show-people/show-people.component.ts
@@ -1,32 +1,24 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PeopleService } from './people.service';
 import { MatDialog } from '@angular/material';
 import { HouseMembersComponent } from '../house-members/house-members.component';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { Person } from '../models/Person';
-import { stringify } from '@angular/compiler/src/util';
 @Component({
   selector: 'app-show-people',
   templateUrl: './show-people.component.html',
   styleUrls: ['./show-people.component.scss']
 })
 export class ShowPeopleComponent implements OnInit {
-  loading: boolean = true
-  displayedColumns = ['name', 'bornDate', 'entryDate'];
-  dataSource: MatTableDataSource<any>
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
   constructor(public personService: PeopleService, public dialog: MatDialog, public router: Router) {
+  }
+  
+  ngOnInit() {
     this.personService.getPersonsRequest()
     .subscribe(
       data => {
         this.personService.people = data
-        this.dataSource = new MatTableDataSource(this.personService.people);
-        this.loading = false
-        console.log('Person ARRAY: ' + stringify(this.personService.people))
-        console.log('DATA SOURCE: ' + stringify(this.dataSource))
       },
       (err: HttpErrorResponse) => {
         if (err.error instanceof Error) {
@@ -40,21 +32,6 @@ export class ShowPeopleComponent implements OnInit {
       }
     )
   }
-
-  ngOnInit() {
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-
-  applyFilter(filterValue: string) {
-    filterValue = filterValue.trim(); // Remove whitespace
-    filterValue = filterValue.toLowerCase(); // Datasource defaults to lowercase matches
-    this.dataSource.filter = filterValue;
-  }
-
 
   openHouseMembersDialog(members: Array<any>): void {
     let dialogRef = this.dialog.open(HouseMembersComponent, {
