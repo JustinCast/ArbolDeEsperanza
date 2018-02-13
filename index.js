fa0983f65561660f46b84daa31e1930568ed6567
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-13 14:02:56 -0600

    deploying

--- index.js
+++ index.js
@@ -45,14 +45,14 @@ const app = express();
 const path = require('path')
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + 'dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
 // pathlocationstrategy
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname +  '/dist/index.html'))
 })
 
