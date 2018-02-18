e051a5d72a1ba14a21f71d1f6f5f5b7d3900fc42
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-17 19:57:52 -0600

    Manejadores de rutas
    Se crearon los métodos manejadores de rutas para la parte de CRUD de un administrador

--- server/routes/AdminRouter.js
+++ server/routes/AdminRouter.js
@@ -21,3 +21,46 @@ function getAdmin(req, res) {
         }
     })
 }
+
+function saveAdmin(req, res) {
+    console.log(req.body)
+    // almacenar en la base de datos
+    console.log(req.body)
+    let admin = new Admin(req.body)
+    admin.save((err, adminStored) => {
+        if(err) res.status(500).send({message: `Error al guardar el usuario administrador: ${err}`})        
+        else{
+            res.status(200).send({admin: adminStored})
+            console.log("Usuario Administrador guardado con éxito")
+        }
+    })
+}
+
+function updateAdmin(req, res) {
+    let adminId  = req.params.adminId
+    let update = req.body
+    console.log(req.body)
+    Admin.findByIdAndUpdate(adminId, update, (err, adminUpdated) => {
+        if(err)
+            res.status(500).send({message: `Error al actualizar el adiministrador: ${err}`})
+        else{
+            console.log("Administrador actualizado correctamente")
+            res.status(201).send({admin: adminUpdated})
+        }
+    })
+}
+
+function deleteAdmin(req, res) {
+    let adminId  = req.params.adminId
+    Admin.findById(adminId, (err, admin) => {
+        if(err)
+            res.status(500).send({message: `Error al encontrar el administrador: ${err}`})
+        else
+            admin.remove( err => {
+                if(err)
+                    res.status(500).send({message: `Error al borrar el administrador: ${err}`})
+                else
+                    res.status(200).send({message: 'El administrador ha sido borrado'})
+            })
+    })
+}
