7394a8c93da93d927f4855043986668737303769
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-17 20:09:54 -0600

    Seteo del api
    Se setearon los demás endpoints que faltaban (los de admin)

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,11 +2,17 @@
 
 const express = require('express')
 const PersonCtrl = require('./PersonRouter')
+const AdminCtrl = require('./AdminRouter')
 const api = express.Router()
 
+// person endpoints
 api.get('/getAllPersons', PersonCtrl.getPersons) // METODO GET
 api.post('/create', PersonCtrl.savePerson) // METODO POST
 api.put('/update/:personId', PersonCtrl.updatePerson) // METODO PUT
 api.delete('/delete/:personId', PersonCtrl.deletePerson) // METODO DELETE
-
+// admin endpoints
+api.get('/getAdmin', AdminCtrl.getAdmin) // METODO GET
+api.post('/createAdmin', AdminCtrl.saveAdmin) // METODO POST
+api.put('/updateAdmin/:adminId', AdminCtrl.updateAdmin) // METODO PUT
+api.delete('/deleteAdmin/:admin', AdminCtrl.deleteAdmin) // METODO DELETE
 module.exports = api
