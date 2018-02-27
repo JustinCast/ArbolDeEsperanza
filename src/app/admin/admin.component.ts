aaca400eed8bbbd2bd4eca303bab19809957836d
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-02-26 21:37:02 -0600

    Admin Router outlet updating

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { Router } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
@@ -9,58 +9,8 @@ import { HttpErrorResponse } from '@angular/common/http';
   styleUrls: ['./admin.component.scss'],
   providers: [UserService]
 })
-export class AdminComponent implements OnInit, AfterViewInit {
-  p: any
-  filter: any = {};
-  selection: any
-  selectionArray = [
-    "Nombre de usuario",
-    "Role"
-  ]
-  constructor(
-    public _userService: UserService,
-    public _router: Router,
-  ) {}
-  ngOnInit() {
-    if(this._userService.users === undefined){
-      this._userService.getUsers()
-        .subscribe(
-          success => {
-            this._userService.users = success
-          },
-          (err: HttpErrorResponse) => {
-            if (err.error instanceof Error) {
-              // Error del lado del cliente
-              console.log('An error occurred:', err.error.message);
-            } else {
-              // The backend returned an unsuccessful response code.
-              // Error del lado del backend
-              console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
-            }
-          }
-        )
-    }
-  }
+export class AdminComponent implements OnInit {
 
-  ngAfterViewInit() {
-    /**
-     * lo siguiente es necesario para quitar el padding por defecto que trae la libreria para paginació
-     */ 
-    let style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = '.zero-padding { padding: 0; }';
-    document.getElementsByTagName('head')[0].appendChild(style);
-    let ul = document.getElementsByClassName("ngx-pagination")
-    ul[0].classList.add('zero-padding')
-  }
-
-  onViewUserDetails(index){
-    localStorage.setItem('viewDetailsUser', JSON.stringify(this._userService.users[index]))    
-    this._router.navigate(['/show-user-details'])
-  }
-
-  onEditUser(index) {
-    localStorage.setItem('editUser', JSON.stringify(this._userService.users[index]));     
-    this._router.navigate(['/edit-user'])
-  }
+  constructor() {}
+  ngOnInit() {}
 }
--- src/app/show-user/show-user.component.ts
+++ src/app/show-user/show-user.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-show-user',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShowUserComponent implements OnInit {
 
-  constructor() { }
-
+  p: any
+  filter: any = {};
+  selection: any
+  selectionArray = [
+    "Nombre de usuario",
+    "Role"
+  ]
+  constructor(
+    public _userService: UserService,
+    public _router: Router,
+  ) {}
   ngOnInit() {
+    if(this._userService.users === undefined){
+      this._userService.getUsers()
+        .subscribe(
+          success => {
+            this._userService.users = success
+          },
+          (err: HttpErrorResponse) => {
+            if (err.error instanceof Error) {
+              // Error del lado del cliente
+              console.log('An error occurred:', err.error.message);
+            } else {
+              // The backend returned an unsuccessful response code.
+              // Error del lado del backend
+              console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+            }
+          }
+        )
+    }
   }
 
+  ngAfterViewInit() {
+    /**
+     * lo siguiente es necesario para quitar el padding por defecto que trae la libreria para paginació
+     */ 
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = '.zero-padding { padding: 0; }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    let ul = document.getElementsByClassName("ngx-pagination")
+    ul[0].classList.add('zero-padding')
+  }
+
+  onViewUserDetails(index){
+    localStorage.setItem('viewDetailsUser', JSON.stringify(this._userService.users[index]))    
+    this._router.navigate(['/show-user-details'])
+  }
+
+  onEditUser(index) {
+    localStorage.setItem('editUser', JSON.stringify(this._userService.users[index]));     
+    this._router.navigate(['/edit-user'])
+  }
 }
