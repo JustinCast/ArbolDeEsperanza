864b82df183cdf048cb678e509bd8730c6118af1
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-03-01 16:03:19 -0600

    Autenticación correcta en la parte de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import {Location} from '@angular/common';
+import { UserService } from '../services/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   actualYear = (new Date()).getFullYear()
+  loginFG: FormGroup
+  err: boolean = false
+  login: any = {}
   constructor(
-    private _location: Location
-  ) { }
+    private _fb: FormBuilder,
+    private userService: UserService
+  ) { 
+    this.loginFG = this._fb.group({
+      "Username": ['', Validators.required],
+      "Password": ['', Validators.required]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    this.userService.getUser(this.login.UserName, this.login.Password)
+    .subscribe(
+      success => {
+        console.log(success)
+      },
+      err => {
+        this.err = true
+      }
+    )
   }
 
 }
