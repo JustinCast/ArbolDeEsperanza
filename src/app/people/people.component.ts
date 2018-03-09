659b30f852613394e979e953a6b26a53027b5e92
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-03-08 23:32:59 -0600

    Minor bug fixes

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
+import { AuthenticationService } from '../services/authentication.service';
+import { User } from '../models/User';
 @Component({
   selector: 'app-people',
   templateUrl: './people.component.html',
   styleUrls: ['./people.component.scss']
 })
 export class PeopleComponent implements OnInit {
-
+  logguedUser: User
   constructor(
-    private _location: Location
+    private _auth: AuthenticationService
   ) { }
 
   ngOnInit() {
+    this.logguedUser = JSON.parse(this._auth.getUser())
   }
 
 }
