59059ba15802e37a3b2aaa67061b8db2e67dbbaa
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-05-08 17:52:12 -0600

    Reviewing

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import 'hammerjs';
 import { MatStepperModule } from '@angular/material/stepper';
-=======
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
->>>>>>> master
 import { 
   MatCardModule,
   MatButtonModule,
@@ -29,13 +24,10 @@ import {
   MatSlideToggleModule,
   MatCheckboxModule
 } from '@angular/material';
-<<<<<<< HEAD
 import { SearchPersonPipe } from '../pipes/search-person.pipe';
 import { SearchUserPipe } from '../pipes/search-user.pipe';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { MatTabsModule } from '@angular/material/tabs';
-=======
->>>>>>> master
 @NgModule({
   imports: [
     CommonModule,
@@ -56,7 +48,6 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatSnackBarModule,
     MatProgressBarModule,
     MatSlideToggleModule,
-<<<<<<< HEAD
     MatCheckboxModule,
     MDBBootstrapModule,
     FormsModule, 
@@ -66,11 +57,6 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatTabsModule
   ],
   exports: [
-=======
-    MatCheckboxModule
-  ],
-  declarations: [
->>>>>>> master
     MatCardModule,
     MatButtonModule,
     MatExpansionModule, 
@@ -88,7 +74,6 @@ import { MatTabsModule } from '@angular/material/tabs';
     MatSnackBarModule,
     MatProgressBarModule,
     MatSlideToggleModule,
-<<<<<<< HEAD
     MatCheckboxModule,
     MDBBootstrapModule,
     FormsModule, 
@@ -104,9 +89,5 @@ import { MatTabsModule } from '@angular/material/tabs';
     SearchPersonPipe,
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
-=======
-    MatCheckboxModule
-  ]
->>>>>>> master
 })
 export class SharedModule { }
