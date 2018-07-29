85af7d9130f461aa628914fc77374b5bc6b15482
Justin Castillo Valladares <justincastillovalladares@gmail.com>
2018-07-29 10:31:16 -0600

    Resolution Component Bug Fixed

--- src/app/people/resolutions/resolutions.component.ts
+++ src/app/people/resolutions/resolutions.component.ts
@@ -1,66 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import {Location} from '@angular/common';
-import { Person } from '../../models/Person';
-import { Resolution } from '../../models/Resolution';
-import { PeopleService } from '../../services/people.service';
-import { Route } from '@angular/compiler/src/core';
-import { ActivatedRoute } from '@angular/router';
-import { HealthService } from '../../services/health.service';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Health } from '../../models/Health';
-import { DateAdapter, MAT_DATE_LOCALE, MAT_DATE_FORMATS } from '@angular/material';
-import { MomentDateAdapter } from '@angular/material-moment-adapter';
-import { TIME_FORMATS } from '../../models/TimeFormats';
+import { Location } from "@angular/common";
+import { Person } from "../../models/Person";
+import { Resolution } from "../../models/Resolution";
+import { PeopleService } from "../../services/people.service";
+import { Route } from "@angular/compiler/src/core";
+import { ActivatedRoute } from "@angular/router";
+import { HealthService } from "../../services/health.service";
+import { HttpErrorResponse } from "@angular/common/http";
+import { Health } from "../../models/Health";
+import {
+  DateAdapter,
+  MAT_DATE_LOCALE,
+  MAT_DATE_FORMATS
+} from "@angular/material";
+import { MomentDateAdapter } from "@angular/material-moment-adapter";
+import { TIME_FORMATS } from "../../models/TimeFormats";
 
 @Component({
-  selector: 'app-resolutions',
-  templateUrl: './resolutions.component.html',
-  styleUrls: ['./resolutions.component.scss'],
+  selector: "app-resolutions",
+  templateUrl: "./resolutions.component.html",
+  styleUrls: ["./resolutions.component.scss"],
   providers: [
-    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE]
+    },
     { provide: MAT_DATE_FORMATS, useValue: TIME_FORMATS }
   ]
 })
 export class ResolutionsComponent implements OnInit {
-  public resolutionDate: Date = new Date()
-  public health: Health
-  public loading: boolean = true
-  personID: string
-  localResolutions: Array<Resolution> = [] // esto es para evitar que se ingresen resoluciones no deseadas
-  checkedStates: Array<boolean> = new Array<boolean>(6)
+  public resolutionDate: Date = new Date();
+  public health: Health;
+  public loading: boolean = true;
+  personID: string;
+  localResolutions: Array<Resolution> = []; // esto es para evitar que se ingresen resoluciones no deseadas
+  checkedStates: Array<boolean> = new Array<boolean>(6);
   constructor(
     public peopleService: PeopleService,
     private route: ActivatedRoute,
     private _location: Location,
     private HealthService: HealthService
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.personID = this.route.snapshot.paramMap.get('id')
-    this.makeHealthRequest()
+    this.personID = this.route.snapshot.paramMap.get("id");
+    this.makeHealthRequest();
   }
 
   makeHealthRequest() {
-    this.HealthService.getHealthByPersonID(this.personID)
-    .subscribe(
+    this.HealthService.getHealthByPersonID(this.personID).subscribe(
       success => {
-        this.health = success[0]
-        console.log(this.health.Need)
-        this.loading = false
+        this.health = success[0];
+        console.log(this.health.Need);
+        this.loading = false;
       },
       (err: HttpErrorResponse) => {
         if (err.error instanceof Error) {
           // Error del lado del cliente
-          console.log('An error occurred:', err.error.message);
+          console.log("An error occurred:", err.error.message);
         } else {
           // The backend returned an unsuccessful response code.
           // Error del lado del backend
-          console.log(`Backend returned code ${err.status}, body was: ${JSON.stringify(err.error)}`)
-          this.HealthService.openSnackBar(`Error al ingresar el documento`, 'Ok', 'red-snackbar')
+          console.log(
+            `Backend returned code ${err.status}, body was: ${JSON.stringify(
+              err.error
+            )}`
+          );
+          this.HealthService.openSnackBar(
+            `Error al ingresar el documento`,
+            "Ok",
+            "red-snackbar"
+          );
         }
       }
-    )
+    );
   }
 
   backClicked() {
@@ -69,31 +84,32 @@ export class ResolutionsComponent implements OnInit {
 
   makeResolution(list) {
     list.selectedOptions.selected.map(item => {
-      this.health.Resolutions.push(new Resolution(item.value, this.resolutionDate))
+      this.health.Resolutions.push(
+        new Resolution(item.value, this.resolutionDate)
+      );
       switch (String(item.value)) {
-        case 'Need_Doctor':
-          this.health.Need.Need_Doctor = false
+        case "Need_Doctor":
+          this.health.Need.Need_Doctor = false;
           break;
-        case 'Need_Ophthalmologist':
-          this.health.Need.Need_Ophthalmologist = false
+        case "Need_Ophthalmologist":
+          this.health.Need.Need_Ophthalmologist = false;
           break;
-        case 'Need_Mammography':
-          this.health.Need.Need_Mammography = false
+        case "Need_Mammography":
+          this.health.Need.Need_Mammography = false;
           break;
-        case 'Need_Dentist':
-          this.health.Need.Need_Dentist = false
+        case "Need_Dentist":
+          this.health.Need.Need_Dentist = false;
           break;
-        case 'Need_Gynecologist':
-          this.health.Need.Need_Gynecologist = false
+        case "Need_Gynecologist":
+          this.health.Need.Need_Gynecologist = false;
           break;
-        case 'Need_Psychologist':
-          this.health.Need.Need_Psychologist = false
-          break;      
+        case "Need_Psychologist":
+          this.health.Need.Need_Psychologist = false;
+          break;
         default:
           break;
       }
     });
-    this.HealthService.updateHealthDoc(this.health)
+    this.HealthService.updateHealthDoc(this.health);
   }
-
 }
